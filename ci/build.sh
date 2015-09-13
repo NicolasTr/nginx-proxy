5a569d41a5ba8f90938f9132670ce244b028d790
Nicolas Trésegnie <nicolas.tresegnie@gmail.com>
2015-09-13 14:12:11 +0100

    Push commit tag

--- ci/build.sh
+++ ci/build.sh
@@ -4,6 +4,7 @@
 find . -exec touch -t 200001010000.00 {} ";"
 
 sudo docker pull quay.io/nicolastr11/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_BRANCH} || true
+sudo docker pull quay.io/nicolastr11/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_SHA1} || true
 
 # Build the image
 sudo docker build -t quay.io/nicolastr11/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_BRANCH} .
--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -ex
 
 sudo docker push quay.io/nicolastr11/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_BRANCH}
+
+sudo docker tag quay.io/nicolastr11/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_BRANCH} quay.io/nicolastr11/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_SHA1}
+sudo docker push quay.io/nicolastr11/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_SHA1}
