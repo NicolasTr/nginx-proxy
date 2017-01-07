b9e66bb06188ae71372cbf4cf67139725800aa9f
Nicolas Trésegnie <nicolas.tresegnie@gmail.com>
2017-01-07 13:03:55 +0000

    Add push

--- ci/build.sh
+++ ci/build.sh
@@ -8,3 +8,8 @@ sudo docker pull quay.io/nicolastr11/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_SHA1} |
 
 # Build the image
 sudo docker build -t quay.io/nicolastr11/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_BRANCH} .
+
+sudo docker push quay.io/nicolastr11/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_BRANCH}
+
+sudo docker tag quay.io/nicolastr11/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_BRANCH} quay.io/nicolastr11/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_SHA1}
+sudo docker push quay.io/nicolastr11/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_SHA1}
